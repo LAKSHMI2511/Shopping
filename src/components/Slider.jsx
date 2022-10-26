be7c19e7ce073ae384a935bd4d93df668c564202
LAKSHMI2511 <47786608+LAKSHMI2511@users.noreply.github.com>
2022-10-26 21:05:01 +0530

    1 file modified

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,41 +3,86 @@ import React from 'react';
 import ArrowLeftOutlinedIcon from '@material-ui/icons/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@material-ui/icons/ArrowRightOutlined';
 
-
 const Container = styled.div`
 height:100vw;
 width:100%;
-background-color: coral
 `;
 
-const Wrapper = styled.div`
- display:flex;
- align-items:center;
- justify-content:center;
+const ArrowContainer = styled.div`
+ height:50px;
+ width:50px;
 `;
 
 const Arrow = styled.div`
- border-radius:50%;
- background-color: white;
  top:0;
  bottom:0;
  margin:auto;
- 
- 
+ display:flex;
+ align-items:center;
+ justify-content:center;
+ border-radius: 50%;
+ background-color: #ff7f7;
+ left: ${(props) => props.direction === 'left' && '10px'};
+ right: ${(props) => props.direction === 'right' && '10px'};
+ cursor: pointer;
+ opacity: 0.5;
+ position: absolute;
 `;
+
+const Wrapper = styled.div`
+display:flex;
+align-items:center;
+ justify-content:center;
+`;
+
+const ImgContainer = styled.div`
+flex:1;
+`;
+
+const Image = styled.img`
+height:200px;
+width:600px;
+`;
+
+const InfoContainer = styled.div`
+flex:1;
+`;
+
+const Title = styled.p`
+
+`;
+
+const Desc = styled.p`
+
+`;
+
+const Button = styled.button`
+
+`;
+
 const Slider = () => {
   return (
     <Container>
-      <Wrapper>
-        <Arrow>
-          <ArrowLeftOutlinedIcon/>
+      <ArrowContainer>
+        <Arrow direction="left">
+          <ArrowLeftOutlinedIcon />
         </Arrow>
-        <Arrow>
-          <ArrowRightOutlinedIcon/>
+        <Wrapper>
+            <ImgContainer>
+              <Image src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALE</Title>
+              <Desc> HURRY UP </Desc>
+              <Button>SHOW MORE</Button>
+            </InfoContainer>
+          </Wrapper>
+        <Arrow direction="right">
+          <ArrowRightOutlinedIcon />
         </Arrow>
-      </Wrapper>
+      </ArrowContainer>
     </Container>
   );
-}
+};
 
 export default Slider;
